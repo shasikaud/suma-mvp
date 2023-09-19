aa8e39bcc75f76f4b245cccc03705faf7bb1e1fc
Shasika Udayanga <shasikaudayanga@gmail.com>
2023-09-19 08:21:53 +0100

    added styling to login page

--- app/login/page.jsx
+++ app/login/page.jsx
@@ -16,6 +16,8 @@ const LoginForm = () => {
         password: ''
     })
 
+    const loginEnabled = formData.username !== '' && formData.password !== ''
+
     const router = useRouter();
 
     const submitForm = async(e) => {
@@ -50,25 +52,36 @@ const LoginForm = () => {
     }
 
     return (
-        <div className="flex p-4 m-4 bg-blue-100 text-sky-400">
-        <form onSubmit={submitForm} className="flex flex-row gap-2">
-            <input 
-            type='text' 
-            placeholder="Username" 
-            name="username"
-            value={formData.username}
-            onChange={e => updateForm(e.target.name, e.target.value)}
-            />
-            <input 
-            type="password" 
-            placeholder="Password" 
-            name="password"
-            value={formData.password}
-            onChange={e => updateForm(e.target.name, e.target.value)}
-            />
-            <button type="submit" onClick={submitForm}>Login</button>
-            <Link href='/register'>Register</Link>
-        </form>
+        <div className="flex p-4 m-4 bg-blue-100 text-slate-500">
+            <form onSubmit={submitForm} className="flex flex-row  justify-center gap-2">
+
+                <input 
+                    type='text' 
+                    placeholder="Username" 
+                    name="username"
+                    value={formData.username}
+                    onChange={e => updateForm(e.target.name, e.target.value)}
+                />
+
+                <input 
+                    type="password" 
+                    placeholder="Password" 
+                    name="password"
+                    value={formData.password}
+                    onChange={e => updateForm(e.target.name, e.target.value)}
+                />
+
+                <button 
+                    type="submit" 
+                    onClick={submitForm} 
+                    disabled={!loginEnabled}
+                    className={loginEnabled ? "border-green-300 bg-green-100 px-2" : "border-slate-50 bg-slate-300 px-2 text-slate-50"}
+                >
+                    Login
+                </button>
+
+                <Link href='/register' className="border-orange-300 bg-orange-100 px-2">Register</Link>
+            </form>
         </div>
     )
 }
