e0f3b5124814795de79e020a9b9198994e5a5bb9
Shasika Udayanga <shasikaudayanga@gmail.com>
2023-09-19 08:09:24 +0100

    added styling register form [wip]

--- app/register/page.jsx
+++ app/register/page.jsx
@@ -7,10 +7,19 @@ import Link from "next/link"
 
 const RegisterForm = () => {
 
+    const formObj = {
+
+    } 
+
     const [formData, setFormData] = useState({
         username: '',
-        password: ''
+        password: '',
+        repeatPassword: ''
     })
+
+    const validEntries = formData.username !== '' && 
+                          formData.password !== '' &&
+                          (formData.password === formData.repeatPassword)
 
     const router = useRouter();
     
@@ -40,15 +49,42 @@ const RegisterForm = () => {
       }
     
       return (
-        <div className="flex justify-center p-4 bg-blue-100 text-sky-400 m-10">
+        <div className="flex justify-center p-4 bg-blue-100 text-black-200 m-10">
           <form onSubmit={submitForm} className="flex flex-col gap-2 m-4 p-4">
+
+            <div className="flex flex-row gap-2">
+              <input 
+                type='text' 
+                placeholder="First Name" 
+                name="firstName"
+                value={formData.firstName}
+                onChange={e => updateForm(e.target.name, e.target.value)}
+              />
+              <input 
+                type='text' 
+                placeholder="Last Name" 
+                name="lastName"
+                value={formData.LastName}
+                onChange={e => updateForm(e.target.name, e.target.value)}
+              />
+            </div>
+
             <input 
-              type='text' 
-              placeholder="Username" 
+              type='email' 
+              placeholder="Your email address" 
               name="username"
               value={formData.username}
               onChange={e => updateForm(e.target.name, e.target.value)}
             />
+
+            <input 
+              type='text' 
+              placeholder="Company name" 
+              name="companyName"
+              value={formData.companyName}
+              onChange={e => updateForm(e.target.name, e.target.value)}
+            />
+
             <input 
               type="password" 
               placeholder="Password" 
@@ -56,9 +92,29 @@ const RegisterForm = () => {
               value={formData.password}
               onChange={e => updateForm(e.target.name, e.target.value)}
             />
-            <button type="submit" onClick={submitForm}>Register</button>
+
+            <input 
+              type="password" 
+              placeholder="Repeat password" 
+              name="repeatPassword"
+              value={formData.repeatPassword}
+              onChange={e => updateForm(e.target.name, e.target.value)}
+            />
+
+            <button 
+              type="submit" 
+              onClick={submitForm} 
+              className={`border-2 rounded-sm ${validEntries ? "border-red-600 bg-red-100" : "bg-slate-300 text-slate-50"}`}
+              disabled={!validEntries}
+            >
+              Register
+            </button>
+
+            <div className="flex justify-center">
+              <h1>Already have an account? <span> <Link href='/login' className="underline">Login</Link></span></h1>
+            </div>
           </form>
-          <h1>Already have an account? <span> <Link href='/login'>Login</Link></span></h1>
+
         </div>
       )
 }
